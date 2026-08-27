3da7b4b9e97a7b71cb2658dc59ba06fbc567b112
fosstinn <fosstinn@gmail.com>
2026-08-26 21:51:55 -0600

    fix: load JWT secret with ConfigService

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -5,17 +5,23 @@ import { PassportModule } from '@nestjs/passport';
 import { JwtModule } from '@nestjs/jwt';
 import { PrismaService } from '../../prisma/prisma.service';
 import { JwtStrategy } from './jwt.strategy';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
     PassportModule,
-    JwtModule.register({
-      secret: process.env.JWT_SECRET || 'secretKey_pizzerialocal',
-      signOptions: { expiresIn: '1d' },
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        secret:
+          configService.get<string>('JWT_SECRET') || 'secretKey_pizzerialocal',
+        signOptions: { expiresIn: '1d' },
+      }),
     }),
   ],
   controllers: [AuthController],
   // ¡AQUÍ ESTÁ LA CLAVE! PrismaService DEBE estar en este arreglo:
-  providers: [AuthService, JwtStrategy, PrismaService], 
+  providers: [AuthService, JwtStrategy, PrismaService],
 })
 export class AuthModule {}
